186552908037430c57a55aa75ae52b23c151c964
Kaihuan Huang <KaihuanHuang@Justins-MBP.attlocal.net>
2022-08-16 21:56:55 -0700

    oop 28

--- 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Letter.js
+++ 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Letter.js
@@ -1,5 +1,31 @@
 class Letter {
-  
+    constructor(char) {
+        this.char = char
+        this.visible = !/[a-z1-9]/i.test(char)
+
+    }
+    getSolution() {
+        return this.char
+    }
+    guess(charGuess) {
+        if (charGuess.toUpperCase() == this.char.toUpperCase()) {
+            this.visible = true
+            return true
+        }
+        return false
+
+    }
+    toString() {
+        if (this.visible == true) {
+            return this.char
+        }
+        else {
+            return '_'
+        }
+    }
+
 }
+
+
 
 module.exports = Letter;
--- 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Word.js
+++ 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Word.js
@@ -1,7 +1,10 @@
 const Letter = require("./Letter");
 
 class Word {
-  
+    constructor(arr) {
+        this.
+    }
+
 }
 
 module.exports = Word;
