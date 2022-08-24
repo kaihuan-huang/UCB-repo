c390a1c06a5bbb9b577e95c6a88d146b2e963098
Kaihuan Huang <KaihuanHuang@Justins-MBP.attlocal.net>
2022-08-23 20:50:47 -0700

    heroku

--- 11-Express/01-Activities/24-Stu_Custom-Middleware/Unsolved/server.js
+++ 11-Express/01-Activities/24-Stu_Custom-Middleware/Unsolved/server.js
@@ -7,6 +7,8 @@ const PORT = 3001;
 
 const app = express();
 
+app.use(clog);
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
