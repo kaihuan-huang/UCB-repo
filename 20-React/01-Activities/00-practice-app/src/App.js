201fdd1b36b1cd6b0b643a3a09329f39fdf15523
kaihuan Huang <huanhuan@kaihuans-MacBook-Pro.local>
2022-10-25 18:44:46 -1000

    React update

--- 20-React/01-Activities/00-practice-app/src/App.js
+++ 20-React/01-Activities/00-practice-app/src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-// We import our HelloDiv from the components folder
-import HelloDiv from "./components/HelloDiv";
+import Display from "./components/Display";
 
-// App is our top-level main component that references other components
 function App() {
-  return <HelloDiv />;
+  return <Display />;
 }
 
 export default App;
--- 20-React/01-Activities/06-Stu_JSX-expressions/Unsolved/src/components/JSXVariables.js
+++ 20-React/01-Activities/06-Stu_JSX-expressions/Unsolved/src/components/JSXVariables.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 // TODO: Create a `name` variable
+const name = 'kaihuan';
+const thoughts = 'My thought is good';
+const nameLen = name.length;
+console.log(nameLen);
+console.log(thoughts);
 
 // TODO: Create a `thoughts` variable
 // This should be a string regarding what you think of React
@@ -11,11 +16,11 @@ function JSXVariables() {
       <div className="container">
         <div className="jumbotron">
           {/* TODO: Add your name variable */}
-          <h1>Hi! My name is (insert name here)</h1>
+          <h1>Hi! My name is {name}</h1>
           {/* TODO: Add your number of letters variable */}
-          <h2>My name has (insert number of letters in name here) letters</h2>
+          <h2>My name has {nameLen} letters</h2>
           {/* TODO: Add your thoughts variable */}
-          <h2>I think React (insert thoughts about React here)</h2>
+          <h2>I think React {thoughts}</h2>
         </div>
       </div>
     </div>
